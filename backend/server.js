637b5a9aaad617a5dfa23b9a183de6e82303310c
Pratibha1995 <pratibhacse20@gmail.com>
2025-11-26 00:43:10 +0530

    Add your own seerver api

---
+++ backend/server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+
+// Basic CORS fix
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+
+app.get('/about',(req,res)=>{
+  const students =  [
+    {
+      id:1,
+      name:"aditya",
+      class:"10"
+    },
+    {
+      id:2,
+      name:"rohan",
+      class:"11"
+    },
+    {
+      id:3,
+      name:"pooja",
+      class:"8"
+    },
+    {
+      id:4,
+      name:"vikash",
+      class:"12"
+    },
+    {
+      id:5,
+      name:"rakesh",
+      class:"5"
+    }
+  ];
+  res.send(students);
+})
+
+app.get('/contact',(req,res)=>{
+  res.send("<h1>this is my first about contact</h1>");
+})
+
+app.listen(PORT,()=>{
+  console.log(`Server is running at http://localhost:${PORT} `)
+})
