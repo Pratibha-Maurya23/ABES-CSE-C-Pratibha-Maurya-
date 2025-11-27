4f85768c7ed761e0a1223e715803ddebf058e025
Pratibha1995 <pratibhacse20@gmail.com>
2025-11-27 20:33:11 +0530

    Add style and use axios instead of fetch

--- CardReactProject/src/App.jsx
+++ CardReactProject/src/App.jsx
@@ -1,25 +1,36 @@
 import { useEffect,useState } from 'react';
+import axios from 'axios';
+import { CgProfile } from "react-icons/cg";
 import './App.css'
 
 function App() {
   const [data,setData] = useState([]);
   useEffect(()=>{
-    fetch("http://localhost:8080/about")
-    .then((res)=>res.json())
-    .then((result)=>{
-      setData(result)
+    // Using fetch
+    // fetch("http://localhost:8080/about")
+    // .then((res)=>res.json())
+    // .then((result)=>{
+    //   setData(result)
+    //   )}
+    axios.get("http://localhost:8080/about")
+    .then((res)=>{setData(res.data)})
+    .catch((err)=>{
+      console.log(err);
     })
-  },[]);
+},[]);
 
-  return ( <div>
-      <h1>Fetched Data</h1>
-      {data.map((item) => (
-          <div key={item.id}>
-    <h3>{item.name}</h3>
-    <p>{item.class}</p>
-  </div>
-      ))}
-    </div>)
+  return ( <div class="container">
+    {data.map((item) => (
+    <div className="box" key={item.id}>
+      <div className="img"><CgProfile  className='imgIcon'/></div>
+      <div className="info">
+        <h2>Name : <span>{item.name}</span></h2>
+        <h3>Class : <span>{item.class}</span></h3>
+      </div>
+    </div>
+     ))}
+  </div>  
+  )
 }
 
 export default App;
